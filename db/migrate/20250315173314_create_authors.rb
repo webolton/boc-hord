ca38763f186df7db44658837cca692fee7f2b30c
William Bolton <wbolton@Williams-MacBook-Pro.local>
2025-03-15 13:35:33 -0400

    Add migration and schema for authors

---
+++ db/migrate/20250315173314_create_authors.rb
@@ -0,0 +1,10 @@
+class CreateAuthors < ActiveRecord::Migration[8.0]
+  def change
+    create_table :authors do |t|
+      t.string :first_name
+      t.string :last_name
+      t.type :integer
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_15_144626) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_15_173314) do
+  create_table "authors", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "entries", force: :cascade do |t|
     t.string "full_title"
     t.datetime "publication_date"
