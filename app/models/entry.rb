8b09fdb624b36c49001f2507a8a1877db8e32ca0
William Bolton <wbolton@Williams-MacBook-Pro.local>
2025-05-09 20:38:34 -0400

    Add relationships and routes for authors

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class Entry < ApplicationRecord
+  has_many :authors, through: :authors_entries
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
   root 'dashboard#index'
 
-  resources :authors
+  resources :authors do
+    collection do
+      get 'new_modal'
+    end
+  end
+
   resources :entries
 end
