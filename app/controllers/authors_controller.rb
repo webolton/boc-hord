965e02f11f0d0bdc3a26a5ac1bcc7d965ec772d2
William Bolton <wbolton@Williams-MacBook-Pro.local>
2025-05-18 20:47:25 -0400

    Add initial form, controller, view, error helper for authors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,7 +1,20 @@
 # frozen_string_literal: true
 
 class AuthorsController < ApplicationController
+  def create
+    @author = Author.new(author_params)
+    if @author.save
+      render new_entry_path, notice: 'Author successfully created.'
+    else
+      render :new_modal, status: :unprocessable_entity
+    end
+  end
+
   def new_modal
     @author = Author.new
+  end
+
+  def author_params
+    params.expect(author: %i[first_name last_name distinction])
   end
 end
